fafeb7c13f450c60a7a4eff0013fddea1f62c7b7
Riccardo Crippa <therickys93@gmail.com>
2025-05-03 11:53:14 +0200

    change Maintenance notification text.

--- server/model/monitor.js
+++ server/model/monitor.js
@@ -1288,6 +1288,7 @@ class Monitor extends BeanModel {
             (previousBeatStatus === DOWN && currentBeatStatus === MAINTENANCE) ||
             (previousBeatStatus === UP && currentBeatStatus === DOWN) ||
             (previousBeatStatus === DOWN && currentBeatStatus === UP) ||
+            (previousBeatStatus === PENDING && currentBeatStatus === MAINTENANCE) ||
             (previousBeatStatus === PENDING && currentBeatStatus === DOWN);
     }
 
@@ -1308,7 +1309,7 @@ class Monitor extends BeanModel {
             } else if (bean.status === DOWN) {
                 text = "🔴 Down";
             } else if (bean.status === MAINTENANCE) {
-                text = "🔵 Maintenance";
+                text = "Under Maintenance";
             }
 
             let msg = `[${monitor.name}] [${text}] ${bean.msg}`;
